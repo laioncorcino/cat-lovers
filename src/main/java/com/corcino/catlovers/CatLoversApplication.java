df4abc86787d357dcdd2e4c4b40a8c4bca01cbc0
Laion Corcino <laioncorcino@gmail.com>
2022-07-19 15:04:04 -0300

    Initial commit

---
+++ src/main/java/com/corcino/catlovers/CatLoversApplication.java
@@ -0,0 +1,13 @@
+package com.corcino.catlovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatLoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatLoversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corcino/catlovers/CatLoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corcino.catlovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatLoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
